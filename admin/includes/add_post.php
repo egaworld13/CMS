ced81862379d117ad91cd692a4dd73e54676db3e
Edgars Roze <edgarsroze13@gmail.com>
2023-05-03 11:21:19 +0300

    post display functionality done

--- admin/includes/add_post.php
+++ admin/includes/add_post.php
@@ -3,7 +3,7 @@
 if (isset($_POST['create_post'])) {
 $post_title = $_POST['post_title'];
 $post_author = $_POST['post_author'];
-$post_category_id = $_POST['post_category_id'];
+$post_category_id = $_POST['post_category'];
 $post_status = $_POST['post_status'];
 
 $post_image = $_FILES['post_image']['name'];
@@ -18,7 +18,7 @@ $post_comment_count = 4;
 move_uploaded_file($post_image_temp, "../images/$post_image");
 
 $query = "INSERT INTO posts(post_category_id, post_title, post_author, post_date, post_image,
- post_content, post_tag, post_comment_count, post_status)";
+ post_content, post_tag, post_comment_count, post_status) ";
 
  $query .= "VALUES ({$post_category_id},'{$post_title}','{$post_author}',now(),
  '{$post_image}','{$post_content}','{$post_tag}','{$post_comment_count}','{$post_status}')";
@@ -37,10 +37,28 @@ confirmQuery($create_post_query);
 <input type="text" class="form-control" name="post_title">
 </div>
 <!-- POST CATEGORY -->
+</br>
 <div class="form-gorup">
-<label for="post_category_id">Posts Category Id</label>
-<input type="text" class="form-control" name="post_category_id">
+<label for="post_category">Posts Category</label></br>
+    <select name="post_category" id="post_category">
+        
+        <?php
+$query = "SELECT * FROM categories";
+$select_categories = mysqli_query($connection, $query);
+
+confirmQuery($select_categories);
+
+while($row = mysqli_fetch_assoc($select_categories)){
+    $cat_id = $row['cat_id'];
+    $cat_title = $row['cat_title'];
+    
+    echo "<option value='{$cat_id}'>{$cat_title}</option>";
+}
+?>
+</select>
+
 </div>
+</br>
 <!-- POST AUTHOR -->
 <div class="form-gorup">
 <label for="post_author">Posts Author</label>
--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -25,7 +25,7 @@
         $post_id = $row['post_id'];
         $post_author = $row['post_author'];
         $post_title = $row['post_title'];
-        $post_category = $row['post_category_id'];
+        $post_category_id = $row['post_category_id'];
         $post_status = $row['post_status'];
         $post_image = $row['post_image'];
         $post_tag = $row['post_tag'];
@@ -36,7 +36,15 @@
         echo "<td>{$post_id}</td>";
         echo "<td>{$post_author}</td>";
         echo "<td>{$post_title}</td>";
-        echo "<td>{$post_category}</td>";
+        
+        $query = "SELECT * FROM categories WHERE cat_id = $post_category_id";
+        $select_categories_to_display = mysqli_query($connection, $query);
+        
+        
+        while($row = mysqli_fetch_assoc($select_categories_to_display)){
+          $cat_title = $row['cat_title'];
+        echo "<td>{$cat_title}</td>";
+        }
         echo "<td>{$post_status}</td>";
         echo "<td> <img width= '100'  src='../images/{$post_image}' alt='img'></td>";
         echo "<td>{$post_tag}</td>";
