30d7360786c962bcdb1fa46dc925f5c3a6b8e6de
Edgars Roze <edgarsroze13@gmail.com>
2023-05-16 11:07:33 +0300

    done with comment functionality

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -10,6 +10,8 @@
                 <th>Tags</th>
                 <th>Comments</th>
                 <th>Date</th>
+                <th>Edit</th>
+                <th>Delete</th>
             </tr>
         </thead>
         
@@ -62,16 +64,10 @@
             $the_post_id = $_GET['delete'];
             $query = "DELETE FROM posts WHERE post_id = {$the_post_id}";
             $delete_query = mysqli_query($connection, $query);
+            header("Location: posts.php");
 
         }
-        if (isset($_GET['delete'])){
-            
-            $the_post_id = $_GET['delete'];
-            $query = "DELETE FROM posts WHERE post_id = {$the_post_id}";
-            $delete_query = mysqli_query($connection, $query);
-
-        }
-        
+           
         ?>
     
         </tbody>
--- index.php
+++ index.php
@@ -12,7 +12,7 @@
             <!-- Blog Entries Column -->
             <div class="col-md-8">
                 <?php
-                $query = "SELECT* FROM posts";
+                $query = "SELECT * FROM posts ";
                 $select_all_posts = mysqli_query($connection,$query);
 
                 while($row = mysqli_fetch_assoc($select_all_posts)){
@@ -22,51 +22,44 @@
                     $post_date = $row['post_date'];
                     $post_image = $row['post_image'];
                     $post_content = substr($row['post_content'],0,100);
-                    // $post_tag = $row['post_title'];
-                    // $post_comment_count = $row['post_title'];
-                    // $post_status = $row['post_title'];
-                    ?>
-                  
-
-
-                  
-                  
-                  <h1 class="page-header">
-                      Page Heading
-                      <small>Secondary Text</small>
-                    </h1>
+                    $post_status = $row['post_status'];
                     
-                    <!-- Blog Post -->
-                    <h2>
-                        <a href="post.php?p_id=<?php echo $post_id;?>"><?php echo $post_title;?></a>
-                    </h2>
-                    <p class="lead">
-                        by <a href="index.php"><?php echo $post_author?></a>
-                    </p>
-                    <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date?></p>
-                    <hr>
-                    <a href="post.php?p_id=<?php echo $post_id;?>">
-                        <img class="img-responsive" src="images/<?php echo $post_image?>" alt="">
-                    </a>
-                    <hr>
-                    <p><?php echo $post_content?></p>
-                    <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-                    
-                    <hr>
+                    if($post_status === 'Published'){
+                        ?>
 
-                <?php };?>
-                
-                
-                
-                
-                
-                
-                
-                
-                
-               
 
-            </div>
+<h1 class="page-header">
+    Page Heading
+    <small>Secondary Text</small>
+  </h1>
+  
+  <!-- Blog Post -->
+  <h2>
+      <a href="post.php?p_id=<?php echo $post_id;?>"><?php echo $post_title;?></a>
+  </h2>
+  <p class="lead">
+      by <a href="index.php"><?php echo $post_author?></a>
+  </p>
+  <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date?></p>
+  <hr>
+  <a href="post.php?p_id=<?php echo $post_id;?>">
+      <img class="img-responsive" src="images/<?php echo $post_image?>" alt="">
+  </a>
+  <hr>
+  <p><?php echo $post_content?></p>
+  <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+  
+  <hr>
+
+                <?php } ?>
+                        
+<?php}
+
+if($post_status !== 'Published'){ ?>
+    <h1 class="text-center"> There is no posts at the moment! </h1>
+<?php }?>
+
+             </div>
 
             <!-- Blog Sidebar Widgets Column -->
             <?php include "includes/sidebar.inc.php"?>
